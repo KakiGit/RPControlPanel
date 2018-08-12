1efbf06daf5aed33e7c73e0a396ae8b6e7a4c812
KakiGit <kaki_github@icloud.com>
2018-08-12 17:29:52 +0800

    use socket for communications

---
+++ main.js
@@ -0,0 +1,88 @@
+let ws
+$.ajax({
+    type: "POST",
+    url: "ajax/stats.php",
+    success: function (result) {
+        $("#ipaddr").text(result)
+        ws = new WebSocket("ws://" + result + ":9011")
+    }
+})
+
+$(function () {
+    $(".btn-trigger").click(function () {
+        let text = $(this).text().replace(/ /g, "").replace(/\n/g, "").replace(/\r/g, "").replace(/\t/g, "")
+        let cmd = ""
+        switch (text) {
+            case "关机":
+                cmd = "sudo shutdown -h now"
+                break
+            case "重启":
+                cmd = "sudo reboot"
+                break
+        }
+        if (confirm("确定要执行该命令吗？")) {
+            ws.send(cmd)
+        }
+    })
+})
+
+$(function () {
+    $("#aria2").click(function () {
+        window.open("./webui-aria2/index.html")
+    })
+    $("#download").click(function () {
+        window.open("./webui-aria2/Downloads/")
+    })
+})
+
+ws.onmessage = function (data) {
+
+    let elements = data.split(' ')
+    $("#Temperature").text(elements[0] + "℃")
+    if (parseInt(elements[0]) >= 60) {
+        $("#Temperature").css("background-color", "#d9534f")
+        $("#Temperature").css("color", "#ffffff")
+    }
+    else {
+        $("#Temperature").css("background-color", "#ffffff")
+        $("#Temperature").css("color", "#000000")
+    }
+    $("#CPU_Usage").text(elements[1] + "%")
+    if (parseInt(elements[1]) >= 80) {
+        $("#CPU_Usage").css("background-color", "#d9534f")
+        $("#CPU_Usage").css("color", "#ffffff")
+    }
+    else {
+        $("#CPU_Usage").css("background-color", "#ffffff")
+        $("#CPU_Usage").css("color", "#000000")
+    }
+    $("#RAM_Avail").text(elements[2] + "M")
+    if (parseInt(elements[2]) <= 200) {
+        $("#RAM_Avail").css("background-color", "#d9534f")
+        $("#RAM_Usage").css("background-color", "#d9534f")
+        $("#RAM_Avail").css("color", "#ffffff")
+        $("#RAM_Usage").css("color", "#ffffff")
+    }
+    else {
+        $("#RAM_Avail").css("background-color", "#ffffff")
+        $("#RAM_Usage").css("background-color", "#ffffff")
+        $("#RAM_Avail").css("color", "#000000")
+        $("#RAM_Usage").css("color", "#000000")
+    }
+    $("#RAM_Usage").text(elements[3] + "%")
+    $("#DISK_Avail").text(elements[4] + "G")
+    if (parseInt(elements[4]) <= 10) {
+        $("#DISK_Avail").css("background-color", "#d9534f")
+        $("#DISK_Usage").css("background-color", "#d9534f")
+        $("#DISK_Avail").css("color", "#ffffff")
+        $("#DISK_Usage").css("color", "#ffffff")
+    }
+    else {
+        $("#DISK_Avail").css("background-color", "#ffffff")
+        $("#DISK_Usage").css("background-color", "#ffffff")
+        $("#DISK_Avail").css("color", "#000000")
+        $("#DISK_Usage").css("color", "#000000")
+    }
+    $("#DISK_Usage").text(elements[5])
+}
+
--- mylistener.py
+++ mylistener.py
@@ -12,6 +12,7 @@ import os
 import linuxcmd
 import time
 from multiprocessing import Value, Array
+import struct
 
 encoding = 'utf-8'
 BUFSIZE = 1024
@@ -34,6 +35,31 @@ def parse_data(msg):
     return ''.join([chr(v ^ mask[k % 4]) for k, v in enumerate(data)])
 
 
+def sendMessage(client, message):
+    msgLen = len(message)
+    backMsgList = []
+    backMsgList.append(struct.pack('B', 129))
+
+    if msgLen <= 125:
+        backMsgList.append(struct.pack('b', msgLen))
+    elif msgLen <= 65535:
+        backMsgList.append(struct.pack('b', 126))
+        backMsgList.append(struct.pack('>h', msgLen))
+    elif msgLen <= (2 ^ 64-1):
+        backMsgList.append(struct.pack('b', 127))
+        backMsgList.append(struct.pack('>h', msgLen))
+    else:
+        print("the message is too long to send in a time")
+        return
+    message_byte = bytes()
+    print(type(backMsgList[0]))
+    for c in backMsgList:
+        message_byte += c
+    message_byte += bytes(message, encoding="utf8")
+
+    client.send(message_byte)
+
+
 def socketServer():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -42,6 +68,7 @@ def socketServer():
     while True:
         client, cltadd = sock.accept()
         data = client.recv(1024)
+        cltadd = cltadd
         header, sub = data.split(str("\r\n\r\n").encode(encoding), 1)
         for line in header.split(str('\r\n').encode(encoding))[1:]:
             key, value = line.split(str(': ').encode(encoding), 1)
@@ -57,6 +84,7 @@ Connection: Upgrade\r\n\
 Sec-WebSocket-Accept: {}\r\n\r\n\
 '.format(bytes.decode(token, encoding))).encode(encoding))
         multiprocessing.Process(target=receiveData, args=(client,)).start()
+        multiprocessing.Process(target=systemPara, args=(client,)).start()
         global connection
         connection.value = True
         print("accept a connect from:", client.getpeername())
@@ -68,6 +96,7 @@ def receiveData(client):
         if(data):
             tmp = parse_data(data)
             print(tmp, "from", client.getpeername())
+            os.system(tmp)
         else:
             break
 
@@ -76,62 +105,35 @@ def receiveData(client):
     print("close:", client.getpeername())
 
 
-def systemPara():
+def systemPara(client):
     global connection
-    while True:
+    while connection.value:
+
+        DISK_stats = linuxcmd.getDiskSpace()
+        DISK_total = DISK_stats[0]
+        DISK_used = DISK_stats[1]
+        DISK_perc = DISK_stats[3]
+        DISK_Avail = float(DISK_total.rstrip('G')) - \
+            float(DISK_used.rstrip('G'))
+        CPU_temp = linuxcmd.getCPUtemperature()
+        CPU_usage = linuxcmd.getCPUuse()
+
+        RAM_stats = linuxcmd.getRAMinfo()
+        RAM_total = round(int(RAM_stats[0]) / 1000, 1)
+        RAM_used = round(int(RAM_stats[1]) / 1000, 1)
+        RAM_Avail = RAM_total - RAM_used
+        RAM_Usage = round(RAM_used / RAM_total, 1)
+        dataToBeSent = CPU_temp + ' ' + CPU_usage + ' ' + \
+            str(RAM_Avail)+' '+str(RAM_Usage)+' ' + \
+            str(DISK_Avail)+' '+str(DISK_perc)+' '
         if connection.value:
-
-            file = open("/var/www/html/ajax/linuxcmd.txt", "r")
-            text = file.read()
-            file.close()
-
-            if(text != ""):
-                    # clear cmd
-                file = open("/var/www/html/ajax/linuxcmd.txt", "w")
-                file.write("")
-                file.close()
-                # print(text)
-                os.system(text)
-
-            DISK_stats = linuxcmd.getDiskSpace()
-            DISK_total = DISK_stats[0]
-            DISK_used = DISK_stats[1]
-            DISK_perc = DISK_stats[3]
-            DISK_Avail = float(DISK_total.rstrip('G')) - \
-                float(DISK_used.rstrip('G'))
-            CPU_temp = linuxcmd.getCPUtemperature()
-            CPU_usage = linuxcmd.getCPUuse()
-            # RAM information
-            # Output is in kb, here I convert it in Mb for readability
-            RAM_stats = linuxcmd.getRAMinfo()
-            RAM_total = round(int(RAM_stats[0]) / 1000, 1)
-            RAM_used = round(int(RAM_stats[1]) / 1000, 1)
-            RAM_Avail = RAM_total - RAM_used
-            RAM_Usage = round(RAM_used / RAM_total, 1)
-
-            with open("/var/www/html/ajax/pistats.txt", "w") as f:
-                # f.write(CPU_temp)
-                f.write(CPU_temp+'\n')
-                f.write(CPU_usage+'\n')
-                f.write(str(RAM_Avail)+'\n')
-                f.write(str(RAM_Usage)+'\n')
-                f.write(str(DISK_Avail)+'\n')
-                f.write(str(DISK_perc)+'\n')
-                f.close()
-        time.sleep(1)
+            sendMessage(client, dataToBeSent)
 
 
 if __name__ == "__main__":
     connection.value = False
-    file = open("/var/www/html/ajax/linuxcmd.txt", "w")
-    file.write("")
-    file.close()
     serv = multiprocessing.Process(target=socketServer)
     serv.start()
-
-    sys = multiprocessing.Process(target=systemPara)
-    sys.start()
-
 # Now, you can use telnet to test it, the command is "telnet 127.0.0.1 9011"
 # You also can use web broswer to test, input the address of "http://127.0.0.1:9011" and press Enter button
 # Enjoy it....
